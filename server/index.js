c5c432633f5564b6be46dd4449880b781d843a8e
Zechariah Deckert-Holscher <zdeckert@gmail.com>
2020-04-30 10:34:40 -0700

    added nodemon, npm start:dev script

--- server/index.js
+++ server/index.js
@@ -3,7 +3,7 @@ const app = express();
 const bodyParser = require('body-parser');
 const port = 3003;
 
-app.use(bodyParser.json();)
+app.use(bodyParser.json());
 app.use(express.static('../client/dist/index.html'))
 
 app.listen(port, ()=> console.log(`recipe-steps listening at http://localhost:${port}`));
