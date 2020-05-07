34a41841ec1abed4c7e5c48e70b9726362176be0
Zechariah Deckert-Holscher <zdeckert@gmail.com>
2020-05-06 18:45:16 -0700

    inital tests

--- database/seeding.js
+++ database/seeding.js
@@ -1,6 +1,5 @@
-const db = require('./index.js');
 const faker = require('faker');
-
+const db = require('./index.js');
 
 const videoUrls = [
   'https://assets.bonappetit.com/clips/5e908f11a19ab80008f8f97e/720p/pass/BA_BAO_S06_Ep048_Basically_CinnamonDateSwirlBuns_01_WARM_OIL_BUTTERMILK_.mp4.mp4',
@@ -62,7 +61,7 @@ const videoUrls = [
   'https://assets.bonappetit.com/clips/5e74f54502b81c00082987c3/720p/pass/BA_BAO_S06_Ep045_Basically_TahiniBillionairesBars_05b_WHISK_CREAM_SALT_REMOVE_.mp4.mp4',
   'https://assets.bonappetit.com/clips/5e74f545b18d1c00086452e2/720p/pass/BA_BAO_S06_Ep045_Basically_TahiniBillionairesBars_05c_ADD_TAHINI_VANILLA_POUR_OVER_SHORTBREAD_.mp4.mp4',
   'https://assets.bonappetit.com/clips/5e74f54604fca90008eaf728/720p/pass/BA_BAO_S06_Ep045_Basically_TahiniBillionairesBars_06_POUR_CHOCOLATE_SESAME_SEEDS_.mp4.mp4',
-  'https://assets.bonappetit.com/clips/5e74f54704fca90008eaf72a/720p/pass/BA_BAO_S06_Ep045_Basically_TahiniBillionairesBars_07_CUT_SERVE_.mp4.mp4'
+  'https://assets.bonappetit.com/clips/5e74f54704fca90008eaf72a/720p/pass/BA_BAO_S06_Ep045_Basically_TahiniBillionairesBars_07_CUT_SERVE_.mp4.mp4',
 ];
 const nonRandomRecipe = {
   id: 1,
@@ -72,81 +71,81 @@ const nonRandomRecipe = {
       id: 1,
       number: 1,
       text: 'Step 1: Combine ¾ cup buttermilk and 6 Tbsp. vegetable oil in a small microwave-safe bowl (it won’t get smooth and that’s okay) and heat in the microwave in three 10-second bursts until just about body temperature (it shouldn’t feel hot or cold to the touch; an instant-read thermometer should register about 98°). If you don’t have a microwave, you can heat in a small saucepan over medium-low, stirring constantly, about 1 minute.',
-      hasVideos: 1
+      hasVideos: 1,
     },
 
     {
       id: 2,
       number: 2,
       text: 'Step 2: Whisk 1 large egg, one ¼-oz. envelope active dry yeast (about 2¼ tsp.), and ¼ cup (packed; 50 g) dark brown sugar in a measuring glass to combine, then whisk into buttermilk mixture.',
-      hasVideos: 1
+      hasVideos: 1,
     },
     {
       id: 3,
       number: 3,
       text: 'Step 3: Pulse 3 cups (375 g) all-purpose flour, 1 tsp. kosher salt, and ¼ tsp. baking soda in a food processor to combine. With the motor running, stream in buttermilk mixture. Process until about 80% of dough comes together into a ball (mixture will look very wet at first, then begin to pull away from the sides of processor), about 2 minutes.',
-      hasVideos: 1
+      hasVideos: 1,
     },
 
     {
       id: 4,
       number: 4,
       text: 'Step 4: Using a bowl scraper or rubber spatula, scrape dough onto an unfloured surface. (It should still be pretty wet and sticky.) Knead, pushing it away from you, then pulling it back toward you, until a smooth ball forms, about 3 minutes. The dough will grow silkier, tighter, and easier to work with as you knead. (Don’t be tempted to add flour; if dough is too sticky to work with, lightly oil your hands.)',
-      hasVideos: 1
+      hasVideos: 1,
     },
     {
       id: 5,
       number: 5,
       text: 'Step 5: Roll out dough into a rough 8" square. Fold dough over onto itself to make an 8x4" rectangle, then flatten it slightly and fold over once more to make a 4" square. Roll dough back out to an 8" square and repeat folding process (you should be back at a 4" square). Pour 1 Tbsp. vegetable oil into a medium bowl and add dough (still folded); turn to coat. Cover bowl tightly and chill dough until doubled in volume, at least 8 hours and up to 1 day.',
-      hasVideos: 1
+      hasVideos: 1,
     },
     {
       id: 6,
       number: 6,
       text: 'Step 6: To make the date filling, bring 1½ cups water to a boil in a small saucepan. Tear 1 cup (packed; 180 g) Medjool dates in half and remove pits; discard. Place dates in a small bowl. Pour hot water over. Let soak until softened, about 10 minutes. Drain dates and transfer to clean food processor; discard soaking liquid. Add 2 Tbsp. vegetable oil, 2 tsp. ground cinnamon, and remaining ½ tsp. kosher salt. Purée, scraping down sides as needed, until smooth, about 4 minutes. (You should have about ½ cup purée.)',
-      hasVideos: 1
+      hasVideos: 1,
     },
     {
       id: 7,
       number: 7,
       text: 'Step 7: Time to assemble the buns! Grease a 10" cast-iron skillet with remaining 1 Tbsp. vegetable oil. Punch down dough, then transfer to a clean, unfloured surface and roll out to an 8" square. Fold in half into an 8x4" rectangle, then fold rectangle over itself to form a 4" square (this is the same folding process you did before). If dough feels tough and uncooperative, let it sit about 5 minutes to relax and try again. Roll out dough to a 12" square about ¼" thick.',
-      hasVideos: 1
+      hasVideos: 1,
     },
     {
       id: 8,
       number: 8,
       text: 'Step 8: Dollop date purée all over. Using a small offset spatula or spoon, spread evenly over dough, leaving a ½" border without purée along edge farthest from you. Sprinkle remaining ¼ cup (packed; 50 g) dark brown sugar over purée.',
-      hasVideos: 1
+      hasVideos: 1,
     },
     {
       id: 9,
       number: 9,
       text: 'Step 9: Starting at edge closest to you, roll dough away from you into a tight log. Using a sharp serrated knife and long sawing motions, trim about ½" of dough from both ends and discard. Slice log crosswise into 3 sections, wiping knife clean between slices. Slice each section crosswise into 3 buns (you should have 9 buns total that are each about 1" thick). Transfer buns to prepared pan as you go. When you first place the buns in the pan, space them evenly. As they proof, the buns will start to touch (this is what you want so that when then bake they rise vertically in the oven.)',
-      hasVideos: 1
+      hasVideos: 1,
     },
     {
       id: 10,
       number: 10,
       text: 'Step 10: Cover pan tightly with foil (or a domed lid if you have one) and place in a warm, dry spot. Let buns rise until they’re doubled in volume and spring back slowly when poked, leaving only a small indentation, 1–1½ hours, depending on how warm and humid your kitchen is. (If your kitchen is cold, you can let the buns rise in a turned-off oven with its light on.)',
-      hasVideos: 1
+      hasVideos: 1,
     },
     {
       id: 11,
       number: 11,
       text: 'Step 11: Place a rack in middle of oven; preheat to 350°. Bake buns, still covered, until puffed, pale, and mostly set, about 20 minutes. Remove foil and continue to bake until golden brown, about 15 minutes if you prefer a soft and squishy bun and up to 25 minutes for a more toasted bun. Let cool slightly.',
-      hasVideos: 0
+      hasVideos: 0,
     },
     {
       id: 12,
       number: 12,
       text: 'Step 12: Meanwhile, whisk ¾ cup (83 g) powdered sugar, 1 tsp. vanilla paste or extract, and remaining 3 Tbsp. buttermilk in a medium bowl to combine. (Vanilla bean paste will give you cute flecks, but extract offers the same flavor.)',
-      hasVideos: 1
+      hasVideos: 1,
     },
     {
       id: 13,
       number: 13,
       text: 'Step 13: Brush glaze over warm buns (warm buns are key so that the glaze can soak into all the crannies) and serve from skillet.',
-      hasVideos: 1
+      hasVideos: 1,
     },
   ],
   videos: [
@@ -205,12 +204,12 @@ const nonRandomRecipe = {
     {
       stepsId: 13,
       url: 'https://assets.bonappetit.com/clips/5e908f1bed51740008882af8/720p/pass/BA_BAO_S06_Ep048_Basically_CinnamonDateSwirlBuns_10b_GLAZE_SERVE_.mp4.mp4',
-    }
-  ]
+    },
+  ],
 };
 
-const dbQuery = (sql, cb = ()=>{}) => {
-  db.con.query(sql, (err, result)=>{
+const dbQuery = (sql, cb = () => {}) => {
+  db.con.query(sql, (err, result) => {
     if (err) {
       console.log(`Database Seeding Error: ${sql}`, err);
     }
@@ -218,8 +217,8 @@ const dbQuery = (sql, cb = ()=>{}) => {
   });
 };
 
-//iife creates database, establishes tables;
-(()=> {
+// iife creates database, establishes tables;
+(() => {
   let sqlString = 'DROP DATABASE IF EXISTS bonappetit;';
   dbQuery(sqlString);
 
@@ -281,22 +280,22 @@ const dbQuery = (sql, cb = ()=>{}) => {
 
 
 // change n for amount of recipes generated
-((n = 100)=> {
-  for (var i = 2; i <= n; i++) {
-    let sqlString = `INSERT INTO recipes (name) VALUES ('${faker.lorem.words(faker.random.number({ min: 1, max: 5 }))}');`;
+((n = 100) => {
+  for (let i = 2; i <= n; i += 1) {
+    const sqlString = `INSERT INTO recipes (name) VALUES ('${faker.lorem.words(faker.random.number({ min: 1, max: 5 }))}');`;
     dbQuery(sqlString);
-    for (var j = 1; j <= faker.random.number({ min: 5, max: 15 }); j++) {
-      let hasVideos = faker.random.number(1);
-      let sqlString = `INSERT INTO steps (recipes_id, number, text, hasVideos) \
+    for (let j = 1; j <= faker.random.number({ min: 5, max: 15 }); j += 1) {
+      const hasVideos = faker.random.number(1);
+      const sqlString1 = `INSERT INTO steps (recipes_id, number, text, hasVideos) \
       VALUES (${i}, ${j}, '${faker.lorem.sentences(faker.random.number({ min: 10, max: 20 }))}', ${hasVideos} );`;
-      dbQuery(sqlString);
+      dbQuery(sqlString1);
       if (hasVideos) {
-        let sqlString = 'SELECT id FROM steps ORDER BY id DESC LIMIT 1;';
-        dbQuery(sqlString, (result) => {
-          let stepId = result[0].id;
-          for (var k = 1; k <= faker.random.number({ min: 1, max: 2 }); k++) {
-            let sqlString = `INSERT INTO videos (steps_id, url) VALUES (${stepId}, '${videoUrls[faker.random.number({ min: 0, max: videoUrls.length - 1})]}');`;
-            dbQuery(sqlString);
+        const sqlString2 = 'SELECT id FROM steps ORDER BY id DESC LIMIT 1;';
+        dbQuery(sqlString2, (result) => {
+          const stepId = result[0].id;
+          for (let k = 1; k <= faker.random.number({ min: 1, max: 2 }); k += 1) {
+            const sqlString3 = `INSERT INTO videos (steps_id, url) VALUES (${stepId}, '${videoUrls[faker.random.number({ min: 0, max: videoUrls.length - 1})]}');`;
+            dbQuery(sqlString3);
           }
         });
       }
