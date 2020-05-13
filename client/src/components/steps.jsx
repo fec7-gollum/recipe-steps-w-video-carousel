efd5789dc1a7c18bf1418802351e0b6a06bb3350
Zechariah Deckert-Holscher <zdeckert@gmail.com>
2020-05-13 13:53:26 -0700

    added step numbers to carousel components

--- client/src/components/steps.jsx
+++ client/src/components/steps.jsx
@@ -6,42 +6,55 @@ import Videos from './videos.jsx';
 
 const Steps = ({
   hidden, number, text, videos,
-}) => (
-  <div>
-    <p>
-      {number}
-      .
-    </p>
-    <li className="steps-list-item">
-      {text}
-      {videos.length === 1
-      && !hidden
-      && (
-      <div className="videos-wrapper">
-          {videos.map((video) => <Videos video={video} />)}
-      </div>
-      )}
-      {videos.length === 2
-      && !hidden
-      && (
-        <div className="videos-wrapper">
-          <div className="carousel-wrapper">
-            {videos.map((video) => <Videos video={video} />)}
-            <div className="carousel-controls">
-              <button type="button">Previous</button>
-              <button type="button">Next</button>
-            </div>
-            <ol className="carousel-indicators-list">
-              <li className="carousel-indicators" />
-              <li className="carousel-indicators" />
-            </ol>
-          </div>
-        </div>
-      )}
-    </li>
-  </div>
-);
+}) => {
+  const onClickNext = (id) => {
+    // document.getElementById(id).classList.add('active');
+    // // classList.remove("active");
+  };
+  const onClickPrev = (id) => {
+    // element.classList.add("active");
+    // classList.remove("active");
+  };
 
+  return (
+    <div className="steps-individual-wrapper">
+      <p className="steps-list-number">
+        {number}
+        .
+      </p>
+      <li className="steps-list-item">
+        <p className="steps-list-text">
+          {text}
+        </p>
+
+        {videos.length === 1
+        && !hidden
+        && (
+        <div className="videos-wrapper">
+            {videos.map((video) => <Videos video={video} />)}
+        </div>
+        )}
+        {videos.length === 2
+        && !hidden
+        && (
+          <div className="videos-wrapper">
+            <div className="carousel-wrapper">
+              {videos.map((video) => <Videos video={video} stepNum={number} />)}
+              <div className="carousel-controls">
+                <button type="button" onClick={onClickNext}>Previous</button>
+                <button type="button" onClick={onClickPrev}>Next</button>
+              </div>
+              <ol className="carousel-indicators-list">
+                <li className="carousel-indicators" />
+                <li className="carousel-indicators" />
+              </ol>
+            </div>
+          </div>
+        )}
+      </li>
+    </div>
+  );
+};
 
 Steps.propTypes = {
   hidden: PropTypes.bool.isRequired,
--- client/src/components/videos.jsx
+++ client/src/components/videos.jsx
@@ -1,23 +1,30 @@
 import React from 'react';
 import { PropTypes } from 'prop-types';
 
-const Videos = ({ video }) => (
-  <div className="carousel-component">
-    <video
-      playsInline
-      autoPlay
-      muted
-      loop
-    >
-      <source
-        src={video.url}
-        type="video/mp4"
-      />
-    </video>
-  </div>
-);
+const Videos = ({
+  stepNum, video,
+}) => {
+  const classNameWithNumber = `carousel-component ${stepNum}`;
+  return (
+    <div className={classNameWithNumber}>
+      <video
+        playsInline
+        autoPlay
+        muted
+        loop
+      >
+        <source
+          className="video-source"
+          src={video.url}
+          type="video/mp4"
+        />
+      </video>
+    </div>
+  );
+};
 
 Videos.propTypes = {
+  stepNum: PropTypes.number.isRequired,
   video: PropTypes.shape({
     steps_id: PropTypes.number.isRequired,
     id: PropTypes.number.isRequired,
